7fc577c077941bb2912eca70365a751fc7dbadc8
fieldlee <ldpfieldlee@gmail.com>
2022-12-04 21:38:26 +0800

    add flush

--- src/crawler/ytb_download_lib.rs
+++ src/crawler/ytb_download_lib.rs
@@ -1,14 +1,16 @@
 use crate::utils::error::Result;
-use std::io::prelude::*;
-use std::fs::File;
 use futures_util::StreamExt;
+use std::fs::File;
+use std::io::prelude::*;
 
-
-pub async fn download(url: String, v_id:String)->Result<String> {
+pub async fn download(url: String, v_id: String) -> Result<String> {
     let client = reqwest::Client::new();
+
     let resp = client.get(url).send().await?;
+
     let file_size = resp.content_length().unwrap();
-    let file_path = format!("/Users/fieldlee/tmp/{}.mp4",v_id);
+
+    let file_path = format!("/Users/fieldlee/tmp/{}.mp4", v_id);
 
     let mut file = File::create(file_path.clone()).unwrap();
 
@@ -16,12 +18,16 @@ pub async fn download(url: String, v_id:String)->Result<String> {
 
     while let Some(item) = stream.next().await {
         match item {
-            Ok(bytes) =>{
-                file.write_all(&bytes)
-            .or(Err(format!("download Error while writing to file")))?;
+            Ok(bytes) => {
+                file.write_all(&bytes).or(Err(format!("download Error while writing to file")))?;
             }
-            Err(e)=> println!("download Error :{:?}",e),
+            Err(e) => println!("download Error :{:?}", e),
         }
+    }
+    let flush_result = file.flush();
+    match flush_result {
+        Ok(_) => println!(""), 
+        Err(e) =>println!("download Error :{:?}", e),
     }
     Ok(file_path)
 }
